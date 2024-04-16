7b4fbe6ce66a7ab2c0d40cdd8768c223b3ae07cb
Ahmer-Siddiqui <siddiquiahmer970@gmail.com>
2024-04-17 03:30:42 +0500

    add styling and make layout

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const { PORT } = require("dotenv").config().parsed;
+const ejsMate = require('ejs-mate');
 const path = require("path");
 const Listing = require("./models/listingModel");
 const methodOverride = require("method-override");
 const { data } = require("./init/data");
-
+   
 //Database connection
 const connectDB = require("./db/connect")();
 
 // template engine config
 app.set("view engine", "ejs");
+app.engine('ejs', ejsMate);
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"public")))
 
 app.use("/listings", require("./routes/listingsRoutes"));
 
